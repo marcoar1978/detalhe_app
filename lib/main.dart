edc70200b076f79981dda164159562f0c0f4d388
Marco <marcoar1978@gmail.com>
2020-05-13 15:51:35 -0300

    inicio do projeto

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(DetalheApp());
+
+class DetalheApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio do Sistema'),
+      ),
+    );
+  }
+}
