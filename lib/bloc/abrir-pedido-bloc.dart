135d4b26c8dad47281c3e1bc4101c89c933f7319
Marco <marcoar1978@gmail.com>
2020-05-23 22:22:36 -0300

    Form abrir pedido

--- lib/bloc/abrir-pedido-bloc.dart
+++ lib/bloc/abrir-pedido-bloc.dart
@@ -1,21 +1,36 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:detalhe_app1/bloc/DadosIniciaisBloc.dart';
 import 'package:detalhe_app1/models/clinica-model.dart';
+import 'package:detalhe_app1/models/dentista-model.dart';
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
 
 class AbrirPedidoBloc extends BlocBase {
   final formKey = GlobalKey<FormState>();
+  final blocDados = BlocProvider.getBloc<DadosIniciaisBloc>();
   BehaviorSubject<Clinica> selectClinicaSubject = BehaviorSubject<Clinica>();
+  BehaviorSubject<List<Dentista>> dentistaPorClinicaSubject =
+      BehaviorSubject<List<Dentista>>();
+
   Clinica selectClinica;
+  Dentista selectDentista;
+  List<Dentista> dentistas;
+  bool visibleSelDentista;
 
   AbrirPedidoBloc() {
     //this.selectClinicaSubject.sink.add(Clinica());
+    this.dentistas = this.blocDados.dentistasSubject.value;
+    this.dentistaPorClinicaSubject.sink.add(this.dentistas);
+    this.visibleSelDentista = false;
   }
 
-  void changedClinica(Clinica c){
+  void changedClinica(Clinica c) {
     this.selectClinica = c;
-
   }
 
-
+  void selDentista(int clinicaId) {
+    List<Dentista> dentistasPorClinica =
+        this.dentistas.where((dentista) => dentista.clinicaId == clinicaId).toList();
+    this.dentistaPorClinicaSubject.sink.add(dentistasPorClinica);
+  }
 }
--- lib/screens/abrir-pedido.dart
+++ lib/screens/abrir-pedido.dart
@@ -2,6 +2,7 @@ import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:detalhe_app1/bloc/DadosIniciaisBloc.dart';
 import 'package:detalhe_app1/bloc/abrir-pedido-bloc.dart';
 import 'package:detalhe_app1/models/clinica-model.dart';
+import 'package:detalhe_app1/models/dentista-model.dart';
 import 'package:flutter/material.dart';
 
 class AbrirPedido extends StatefulWidget {
@@ -31,7 +32,14 @@ class _AbrirPedidoState extends State<AbrirPedido> {
         }).toList(),
         onChanged: (c) {
           setState(() {
+            blocAbrirPedido.visibleSelDentista = false;
+          });
+          Future.delayed(Duration(milliseconds: 500));
+          this.blocAbrirPedido.selectDentista = null;
+          this.blocAbrirPedido.selDentista(c.id);
+          setState(() {
             this.blocAbrirPedido.selectClinica = c;
+            blocAbrirPedido.visibleSelDentista = true;
           });
         },
         validator: (value) {
@@ -42,6 +50,49 @@ class _AbrirPedidoState extends State<AbrirPedido> {
         },
       ),
     );
+  }
+
+  Widget _selDentista(BuildContext context){
+    return StreamBuilder(
+      stream: this.blocAbrirPedido.dentistaPorClinicaSubject.stream,
+      builder: (context, snapshot){
+        if(snapshot.hasData){
+          List<Dentista> dentistas = snapshot.data;
+          return AnimatedOpacity(
+            opacity: blocAbrirPedido.visibleSelDentista ? 1.0 : 0.0,
+            duration: Duration(milliseconds: 500),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: DropdownButtonFormField<Dentista>(
+                hint: Text('Selecione o dentista'),
+                value: this.blocAbrirPedido.selectDentista,
+                items: dentistas.map((Dentista d){
+                  return DropdownMenuItem<Dentista>(
+                    value: d,
+                    child: Text(d.nome),
+                  );
+                }).toList(),
+                onChanged: (d){
+                  setState(() {
+                    this.blocAbrirPedido.selectDentista = d;
+                  });
+                },
+                validator: (value){
+                  if(value == null){
+                    return 'Escolha o dentista';
+                  }
+                  return '';
+                },
+
+              ),
+            ),
+          );
+        }
+        return Text('');
+      },
+
+    );
+
   }
 
   @override
@@ -60,6 +111,7 @@ class _AbrirPedidoState extends State<AbrirPedido> {
               height: 16,
             ),
             this._selClinica(context),
+            this._selDentista(context)
           ],
         ),
       ),
